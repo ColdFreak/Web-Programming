8291317e01ea3e128c5b99552526b5b139b6b091
Wang Zhijun <madfrogme@gmail.com>
2014-07-29 01:01:28 +0900

    ポートとアドレスなどの設定を外部ファイルに移す

--- Nodejs/server.js
+++ Nodejs/server.js
@@ -4,6 +4,14 @@ var http = require('http');
 // サーバーを作ってあげる
 var server = http.createServer();
 
+// server.listen()に使われているport とaddressは
+// 外部ファイルのに移行して，requireを使って，
+// 設定ファイルを読み込む形で使う
+// settingsファイルの中にはexportsを使って
+// 設定している
+var settings = require('./settings');
+console.log(settings);
+
 // サーバーに対して，「リクエストを飛んで来たら
 // ．．．をしなさいというイベントを結びつける
 // イベントを結びつけるにはonという命令を使う
@@ -19,5 +27,5 @@ server.on('request', function(req, res) {
 	res.end();
 });
 
-server.listen(1337, '192.168.100.100');
+server.listen(settings.port, settings.host);
 console.log("server listening...");
