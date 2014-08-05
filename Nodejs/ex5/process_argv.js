f53181cde30e1b5728706dc272fe4dada4c5fca4
Wang Zhijun <wang@it-tera.com>
2014-08-05 13:26:34 +0900

    process.argv.forEach

---
+++ Nodejs/ex5/process_argv.js
@@ -0,0 +1,15 @@
+// processはグローバルオブジェクトで、どこからでアクセスできる
+// processはEventEmitterのインスタンスである
+process.argv.forEach(function(val, index, array) {
+    console.log(index + ': ' + val);
+});
+
+//使用例
+//$ node process_argv.js one two=three four
+//0: node
+//1: /home/vagrant/Web-Programming/Nodejs/ex5/process_argv.js
+//2: one
+//3: two=three
+//4: four
+
+
