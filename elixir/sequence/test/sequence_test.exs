0baadf5950dd07c84371919607cf11a967a15456
Wang Zhijun <wang@gmail.com>
2014-08-24 00:53:49 +0900

    elixir exercise sequence

--- elixir/sequence/test/sequence_test.exs
+++ elixir/sequence/test/sequence_test.exs
@@ -1,7 +1,14 @@
 defmodule SequenceTest do
   use ExUnit.Case
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  test "next number " do
+    {:ok, pid} = GenServer.start_link(Sequence.Server, 50)
+    assert GenServer.call(pid, :next_number) == 50
+    assert GenServer.call(pid, :next_number) == 51
+  end
+
+  test "add delta to number " do
+    {:ok, pid} = GenServer.start_link(Sequence.Server, 50)
+    assert GenServer.cast(pid, {:increment_number, 10}) == :ok
   end
 end
