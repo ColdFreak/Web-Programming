2fe1e83af4141b4805b7a94926f84c38ec729ad4
Wang Zhijun <wang@gmail.com>
2014-09-13 22:38:20 +0900

    tail recursiveでodds_and_evensを書く

--- Erlang/ex6/odds_and_evens.erl
+++ Erlang/ex6/odds_and_evens.erl
@@ -7,5 +7,16 @@ odds_and_evens(L) ->
     Evens = [X || X <- L ,  (X rem 2) =:= 0],
     {Odds, Evens}.
     
+odds_and_evens_acc(L) ->
+    odds_and_evens_acc(L, [], []).
+
+odds_and_evens_acc([H | T], Odds, Evens) -> 
+    case (H rem 2) of
+        1 -> odds_and_evens_acc(T, [H | Odds], Evens);
+        0 -> odds_and_evens_acc(T, Odds, [H | Evens])
+    end;
+odds_and_evens_acc([], Odds, Evens) -> 
+    
+    {lists:reverse(Odds), lists:reverse(Evens)}.
 
 
