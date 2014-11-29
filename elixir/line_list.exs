70b5e77169f73e5cb9cc3b5373b5544182fe41ea
Wang Zhijun <madfrogme@gmail.com>
2014-11-29 17:37:44 +0900

    read lines from file

---
+++ elixir/line_list.exs
@@ -0,0 +1,15 @@
+defmodule ProcessFile do
+  def to_lines(fd, result \\ []) do
+    case IO.read(fd, :line) do
+      :eof ->
+        result
+      data ->
+        to_lines(fd, [data | result])
+    end
+  end
+end
+
+fd = File.open!("line_list.exs")
+li = ProcessFile.to_lines(fd)
+IO.puts(Enum.reverse(li))
+
