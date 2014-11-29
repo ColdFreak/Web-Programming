264f8c2d0f0b289cd551e95143300ceeebcd7231
Wang Zhijun <madfrogme@gmail.com>
2014-11-29 16:48:19 +0900

    htmlcomments.exs

--- elixir/htmlcomments.exs
+++ elixir/htmlcomments.exs
@@ -6,26 +6,29 @@ defmodule HtmlComments do
   # コメントの前半と後半を分けて処理する
   # '<!--'をマッチングできるときにstripを開始する.'-->'が見つかるまで，stripし続ける
   # '-->'が見つかるとまた_copyに戻る，もう一回同じ作業する
-  def _copy([], result),                       do: Enum.reverse(result) 
-  def _copy([?<, ?!, ?-, ?- | rest], result),  do: _strip(rest, result)
-  def _copy([ch | rest], result),              do: _copy(rest, [ch | result])
+  defp _copy([], result),                       do: Enum.reverse(result) 
+  defp _copy([?<, ?!, ?-, ?- | rest], result),  do: _strip(rest, result)
+  defp _copy([ch | rest], result),              do: _copy(rest, [ch | result])
 
-  def _strip([], _result),                     do: raise "comment not closed"
-  def _strip([?-, ?-, ?> | rest], result),     do: _copy(rest, result)
-  def _strip([_ch | rest], result),            do: _strip(rest, result)
+  defp _strip([], _result),                     do: raise "comment not closed"
+  defp _strip([?-, ?-, ?> | rest], result),     do: _copy(rest, result)
+  defp _strip([_ch | rest], result),            do: _strip(rest, result)
+
+  @doc """
+  ファイルを開いて，各種処理をする
+  """
+  def process(file) do
+    case File.read(file) do
+      {:ok, binary} ->
+        # IO.puts is_binary body
+        binary 
+        |> to_char_list
+        |> HtmlComments.uncomment
+        |> IO.puts
+      {:error, reason} ->
+        IO.puts "Cannot read file: #{reason}"
+    end
+  end
+
 end
-
-case File.read("test.html") do
-  {:ok, binary} ->
-    binary 
-    |> to_char_list
-    |> HtmlComments.uncomment
-    |> IO.puts
-    # IO.puts HtmlComments.uncomment(to_char_list(binary))
-    # IO.puts is_binary body
-  {:error, reason} ->
-    IO.puts "Cannot read file: #{reason}"
-end
-
-     
-# IO.puts HtmlComments.uncomment('<!-- this is a test --> also a test\n')
+HtmlComments.process("test.html")
