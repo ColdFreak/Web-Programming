3a269e8e5eabeb4df61928f8debbde9ac3cc7582
Wang Zhijun <madfrogme@gmail.com>
2014-12-01 22:36:00 +0900

    blank.ex

---
+++ elixir/blank.ex
@@ -0,0 +1,36 @@
+defprotocol Blank do
+  @doc "Returns true if data is considered blank/empty"
+  def blank?(data)
+end
+defimpl Blank, for: Integer do
+  def blank?(_), do: false
+end
+
+defimpl Blank, for: List do
+  def blank?([]), do: true
+  def blank?(_), do: false
+end
+
+defimpl Blank, for: Map do
+  # %{}とパターンマッチングできないので
+  # mapのサイズをチェックすることは可能
+  def blank?(map), do: map_size(map) == 0
+end
+
+defimpl Blank, for: Atom do
+  def blank?(false), do: true
+  def blank?(nil), do: true
+  def blank?(_), do: false
+end
+
+@doc """
+protocolsとstruct一緒に使うと強力のようで
+iex(1)> defmodule User do
+  ...(1)>   defstruct name: "john", age: 27
+  ...(1)> end
+iex(2)> Blank.blank?(%User{})
+false
+"""
+defimpl Blank, for: User do
+  def blank?(_), do: false
+end
