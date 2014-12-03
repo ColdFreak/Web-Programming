8c507ef25dfad822e85805e4c55978cd16b1f164
Wang Zhijun <wang@it-tera.com>
2014-12-03 11:04:33 +0000

    pmap.exs

---
+++ elixir/pmap.exs
@@ -0,0 +1,28 @@
+defmodule Parallel do
+  @doc"""
+  初めてのselfをmeに代入して、
+  meはpmapを実行するプロセスで、例えばiex shellのこと
+  n個のプロセスで提供された関数で計算した結果を同時にsend命令を実行する
+  spawn_linkの中にselfずっと変わる
+  n個のプロセスリストを最後の二個目のEnum.mapに渡す
+  receiveのほうは別にn個のプロセスと立ち上げて、返事を待つわけじゃない
+  meプロセスの中に返事を待ちうけ
+  """
+  require Logger
+  def pmap(collection, fun) do
+    me = self
+    Logger.info inspect self
+    collection
+    |> 
+    Enum.map(fn(elem) -> spawn_link(fn -> send me, {self, fun.(elem)} end ) end)
+    |>
+    Enum.map(
+              fn(pid) -> 
+                receive do
+                    {^pid, result} ->
+                        result
+                end
+    end)
+                    
+  end
+end
