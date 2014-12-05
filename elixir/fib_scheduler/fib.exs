033a70db9fa499d2d772799a6c6a9ec778cd3016
Wang Zhijun <wang@gmail.com>
2014-12-05 10:08:58 +0900

    fib.exs

--- elixir/fib_scheduler/fib.exs
+++ elixir/fib_scheduler/fib.exs
@@ -18,12 +18,19 @@ end
 defmodule Scheduler do
   def run(num_processes, module, func, to_calculate) do
     (1..num_processes)
+    # 準備OKのプロセスリストを作成
     |> Enum.map(fn(_) -> spawn(module, func, [self]) end)
     |> schedule_processes(to_calculate, [])
   end
 
-  defp schedle_processes(processes, queue, results) do
+  @doc """
+  processesは準備できたプロセスのリスト
+  queue は[37, 37, 37 ]のような処理必要のリスト
+  """
+  defp schedule_processes(processes, queue, results) do
     receive do
+      # 準備できたよみたいメッセージが飛んできたら
+      # 未計算の値どんどん準備できたプロセスに投げる
       {:ready, pid} when length(queue) > 0 ->
         [ next|tail ] = queue
       send pid, {:fib, next, self}
@@ -34,11 +41,25 @@ defmodule Scheduler do
         if length(processes) > 1 do
           schedule_processes(List.delete(processes, pid), queue, results)
         else
-          Enum.sort(result, fn {n1, _}, {n2, _} -> n1 <= n2 end)
+          Enum.sort(results, fn {n1, _}, {n2, _} -> n1 <= n2 end)
         end
      
+      # 未計算の値を投げていく途中に計算された値が飛んできたら、結果をresultsに追加(新しいステータス)
       {:answer, number, result, _pid} ->
-        schedule_processes(process, queue, [{number, result} | results])
+        schedule_processes(processes, queue, [{number, result} | results])
     end
   end
+end
+
+to_process = [ 37, 37, 37, 37, 37, 37, 37, 37, 37, 37, 37 ]
+# run()関数をn回走らせる, 毎回to_processリストすべて処理する?
+# そして毎回違う数のプロセスで処理する？
+Enum.each 1..11, fn num_processes ->
+  {time, result} = :timer.tc(Scheduler, :run, [num_processes, FibSolver, :fib, to_process])
+
+  if num_processes == 1 do
+    IO.puts inspect result
+    IO.puts "\n # time(s)"
+  end
+  :io.format "~2B       ~.2f~n", [num_processes, time/1000000.0]
 end
