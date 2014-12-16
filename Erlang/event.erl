1f90c73d2f997c5585f45d1d48ab0ed3366f217a
Wang Zhijun <wang@it-tera.com>
2014-12-16 11:21:43 +0000

    event.erl

--- Erlang/event.erl
+++ Erlang/event.erl
@@ -3,6 +3,32 @@
 -record(state, {server,
                 name="",
                 to_go=0}).
+start(EventName, Delay) ->
+    % リストはinitに渡すパラメータ
+    spawn(?MODULE, init, [self(), EventName, Delay]).
+
+start_link(EventName, Delay) ->
+    spawn_link(?MODULE, init, [self(), EventName, Delay]).
+
+init(Server, EventName, Delay) ->
+    loop(#state{ server=Server
+                ,name=EventName
+                ,to_go=normalize(Delay) }
+        ).
+cancel(Pid) ->
+    % モニターを使って、Pidまた生きてるかを知る
+    % monitor(process, Item) -> MonitorRef
+    % Pidが死んだら、'DOWN'メッセージがモニタリングしているプロセスに送る
+    Ref = erlang:monitor(process, Pid),
+    Pid ! {self(), Ref, cancel},
+    receive
+        {Ref, ok} ->
+            % flushの役割は{_, MonitorRef, _, _, _}メッセージを取り除くこと
+            erlang:demonitor(Ref, [flush]),
+            ok;
+        {'DOWN', Ref, process, Pid, _Reason} ->
+            ok
+    end.
 loop(S = #state{server=Server, to_go=[T|Next]}) ->
     receive
         {Server, Ref, cancel} ->
