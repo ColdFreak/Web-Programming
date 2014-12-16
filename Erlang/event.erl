358956e4acffde85677fde998e6aa823c024d736
Wang Zhijun <madfrogme@gmail.com>
2014-12-16 22:54:22 +0900

    event.erl

--- Erlang/event.erl
+++ Erlang/event.erl
@@ -3,17 +3,17 @@
 -record(state, {server,
                 name="",
                 to_go=0}).
-start(EventName, Delay) ->
+start(EventName, DateTime) ->
     % リストはinitに渡すパラメータ
-    spawn(?MODULE, init, [self(), EventName, Delay]).
+    spawn(?MODULE, init, [self(), EventName, DateTime]).
 
-start_link(EventName, Delay) ->
-    spawn_link(?MODULE, init, [self(), EventName, Delay]).
+start_link(EventName, DateTime) ->
+    spawn_link(?MODULE, init, [self(), EventName, DateTime]).
 
-init(Server, EventName, Delay) ->
+init(Server, EventName, DateTime) ->
     loop(#state{ server=Server
                 ,name=EventName
-                ,to_go=normalize(Delay) }
+                ,to_go=time_to_go(DateTime) }
         ).
 cancel(Pid) ->
     % モニターを使って、Pidまた生きてるかを知る
@@ -46,6 +46,16 @@ loop(S = #state{server=Server, to_go=[T|Next]}) ->
 normalize(N) ->
     Limit = 49*24*60*60,
     [N rem Limit | lists:duplicate(N div Limit, Limit)].
+
+time_to_go(TimeOut={{_, _, _ },{ _, _, _ }}) ->
+    Now = calendar:local_time(),
+    ToGo = calendar:datetime_to_gregorian_seconds(TimeOut) - calendar:datetime_to_gregorian_seconds(Now),
+    Secs = if ToGo > 0 -> ToGo;
+        ToGo =< 0 -> 0
+    end,
+    normalize(Secs).
+
+
 % 例
 % 1> c(event).
 % {ok,event}
