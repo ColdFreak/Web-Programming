78a4a0fd385095e3bb488f1a6882a3f8dfd5051d
Wang Zhijun <wang@gmail.com>
2014-12-21 00:02:59 +0900

    逆ポーランド法

---
+++ Erlang/calc.erl
@@ -0,0 +1,20 @@
+-module(calc).
+
+-export([rpn/1]).
+
+rpn(L) when is_list(L) ->
+    [Res] = lists:foldl(fun rpn/2, [], string:tokens(L, " ")),
+    Res.
+
+read(N) ->
+    case string:to_float(N) of
+        {error, no_float} ->
+            list_to_integer(N);
+        {F, _} ->
+            F
+    end.
+
+rpn("+", [N1, N2|S]) ->
+    [N1+N2|S];
+rpn(X, Stack) ->
+    [read(X) | Stack].
