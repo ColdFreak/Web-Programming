f2eace1207b9bd0bfdcdd849e21a3234e13823a7
Wang Zhijun <wang@it-tera.com>
2015-02-23 02:37:16 +0000

    euler problem 5

--- Erlang/lcm.erl
+++ Erlang/lcm.erl
@@ -1,14 +1,17 @@
 #!/usr/local/bin/escript
 
 main(_Args) ->
-        lcm(-3,4).
- 
+    lcm_multiple(20).
+
 gcd(A, 0) -> 
         A;
- 
 gcd(A, B) -> 
         gcd(B, A rem B).
  
 %% gcd(A, B)*lcm(A, B) = A*B
 lcm(A,B) ->
-        io:format("~p~n", [abs(A*B div gcd(A,B))]).
+    abs(A*B div gcd(A,B)).
+
+lcm_multiple(N) ->
+    Res = lists:foldl(fun(A,B) -> lcm(A,B) end, 1, lists:seq(1,N)),
+    io:format("~p~n", [Res]).
