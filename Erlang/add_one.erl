19cf6f6f17ed1d6e1acacabb47183b119f80121b
Wang Zhijun <wang@it-tera.com>
2015-02-27 07:36:29 +0000

    add_one.erl

--- Erlang/add_one.erl
+++ Erlang/add_one.erl
@@ -4,8 +4,8 @@
 
 start() ->
     process_flag(trap_exit, true),
-    Pid = spawn_link(add_one, loop, []),
     % Pid = spawn(add_one, loop, []),
+    Pid = spawn(add_one, loop, []),
     register(add_one,Pid ),
     {ok, Pid}.
 
@@ -24,6 +24,26 @@ loop() ->
             Pid ! {result, Msg+1}
     end,
     loop().
+
+%% 使い方
+%% 2> add_one:start().
+%% {ok,<0.37.0>}
+%% 3> add_one:request(3).
+%% 4
+%% 4> add_one:request(5).
+%% 6
+%% 5> add_one:request(one).
+%% {error,{badarith,[{add_one,loop,0,
+%%                            [{file,"add_one.erl"},{line,24}]}]}}
+%% 6>
+%% =ERROR REPORT==== 27-Feb-2015::07:11:53 ===
+%% Error in process <0.37.0> with exit value: {badarith,[{add_one,loop,0,[{file,"add_one.erl"},{line,24}]}]}
+%% 
+%% 
+%% 6> add_one:request(5).
+%% ** exception error: bad argument
+%%      in function  add_one:request/1 (add_one.erl, line 13)
+
 
 %% You have 3 idioms:
 %% 
