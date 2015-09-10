84eb6568dbcd536ec40c2f1d596470959807dbef
Wang Zhijun <so@idcf.jp>
2015-09-10 17:42:07 +0900

    httpoisonの使いかた

--- elixir/githubapi/lib/githubapi.ex
+++ elixir/githubapi/lib/githubapi.ex
@@ -1,8 +1,14 @@
 defmodule Git do
   require Logger
-  def get_zen(url) do
+  def get_zen() do
     # HTTPoison.start
-    res = HTTPoison.get!(url)
-    Logger.info "#{inspect res}"
+    # res = HTTPoison.get!(url)
+    url = "https://api.github.com/authorizations"
+    # body = HTTPoison.post!(url, {:form, [scopes: "repo, user", note: "getting-started"]},  %{"X-GitHub-OTP" => "610554"}) 
+    # json = %{scopes: "repo, user", note: "getting-started"}
+    # |> Poison.encode!
+    # response = HTTPoison.post!(url, json ,  %{"X-GitHub-OTP" => "610554"}) 
+    response = HTTPoison.post!(url, "{\"scopes\": \"repo, user\", \"note\": \"getting-started\"}",  %{"X-GitHub-OTP" => "610554"})
+    Logger.info "#{inspect response}"
   end
 end
