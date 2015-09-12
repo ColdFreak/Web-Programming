044649d8e09c65cf3339e4c633a96e964e04b0fc
Wang Zhijun <wang@gmail.com>
2015-09-12 23:42:55 +0900

    githubapi

--- elixir/githubapi/lib/githubapi.ex
+++ elixir/githubapi/lib/githubapi.ex
@@ -1,4 +1,4 @@
-defmodule Git do
+defmodule Github do
   require Logger
   @doc ~S""" 
   実行したいコマンド、githubのtokenを発行
@@ -8,7 +8,61 @@ defmodule Git do
 
   %HTTPoison.Response{body: "{\"message\":\"Requires authentication\",\"documentation_url\":\"https://developer.github.com/v3/oauth_authorizations/#oauth-authorizations-api\"}", headers: [{"Server", "GitHub.com"}, {"Date", "Thu, 10 Sep 2015 23:01:28 GMT"}, {"Content-Type", "application/json; charset=utf-8"}, {"Content-Length", "138"}, {"Status", "401 Unauthorized"}, {"X-RateLimit-Limit", "60"}, {"X-RateLimit-Remaining", "55"}, {"X-RateLimit-Reset", "1441928569"}, {"X-GitHub-Media-Type", "github.v3; forma t=json"}, {"X-XSS-Protection", "1; mode=block"}, {"X-Frame-Options", "deny"}, {"Content-Security-Policy", "default-src 'none'"}, {"Access-Control-Allow-Credentials", "true"}, { "Access-Control-Expose-Headers", "ETag, Link, X-GitHub-OTP, X-RateLimit-Limit, X-RateLimit-Remaining, X-RateLimit-Reset, X-OAuth-Scopes, X-Accepted-OAuth-Scopes, X-Poll-Interval"}, {"Access-Control-Allow-Origin", "*"}, {"X-GitHub-Request-Id", "D2A8249D:1A01:B33F998:55F20BC7"}, {"Strict-Transport-Security", "max-age=31536000; includeSubdomains; preload"}, {"X-Content-Type-Options", "nosniff"}], status_code: 401}
   """
-  def get_zen() do
+  def zen do
+    url = "https://api.github.com/zen"
+    response = HTTPoison.get!(url)
+    # Logger.info "レスポンス: #{inspect response}"
+
+    # body = response.body
+    # Logger.info "body: #{inspect body}"
+    
+    headers = response.headers
+    # Logger.info "header: #{inspect headers}"
+    Enum.map(headers, fn {header, value} -> IO.puts "#{header}: #{value}\n" end)
+
+   
+  end
+
+  def list_repos do
+    url = "https://api.github.com/user/repos"
+    token = "43b9cd35c0b948654d009c74a5b51fe5f71e797a"
+    headers = [{"Authorization", "token #{token}"}]
+    response = HTTPoison.get!(url, headers)
+    Logger.info "response: #{inspect response.body}"
+
+  end
+  
+  def create_issue do
+    url = "https://api.github.com/repos/ColdFreak/ppool/issues"
+    token = "43b9cd35c0b948654d009c74a5b51fe5f71e797a"
+    # headers = %{"Authorization" => "token #{token}"}
+    response = HTTPoison.post!(url,  "{\"title\": \"Add variable\", \"body\": \"Add max overflow\", \"labels\": [\"design\", \"feature\"\]}", %{"Authorization" => "token #{token}"})
+    Logger.info "response: #{inspect response}"
+  end
+
+  def create_issue_2 do
+    url       = "https://api.github.com/repos/ColdFreak/ppool/issues"
+    token     = "43b9cd35c0b948654d009c74a5b51fe5f71e797a"
+
+    headers   = [{"Authorization", "token #{token}"}]
+    json_data = %{title: "test issue", body: "test body", labels: ["design", "feature"]}
+                |> Poison.encode!
+
+    response = HTTPoison.post!(url,  json_data, headers)
+    Logger.info "response: #{inspect response}"
+  end
+  def create_issue_3 do
+    url       = "https://api.github.com/repos/ColdFreak/ppool/issues"
+    token     = "43b9cd35c0b948654d009c74a5b51fe5f71e797a"
+
+    headers   = [{"Authorization", "token #{token}"}]
+    post_data = {:multipart, [{"title", "test issue 3"}, {"body", "test body 3"}, {"labels", "design"}]}
+
+    response = HTTPoison.post!(url,  post_data, headers)
+    Logger.info "response: #{inspect response}"
+  end
+
+  def token do
     # HTTPoison.start
     # res = HTTPoison.get!(url)
     url = "https://api.github.com/authorizations"
